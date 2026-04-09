b16ad0697281377833e95cbfee017a68849b27b0
alketarexhaj19-ux <your.alketarexhaj19@gmail.com>
2026-04-09 19:26:43 +0100

    Added add_student function with input and error handling

--- file 1.py
+++ file 1.py
@@ -15,4 +15,19 @@ def load_data():
             students = json.load(file)
     except:
         students = {}
+         #2. add_student
+        # GeeksforGeeks Tkinter tutorial
+        def add_student():
+            name = simpledialog.askstring("Input", "Enter student name:")
+            if not name:
+                return
+            if name in students:
+                messagebox.showerror("Error", "Student already exists")
+                return
+            students[name] = { "marks": [], "average": 0 }
+            update_table()
+            
 
+
+
+ 
--- student.py.py
+++ student.py.py
@@ -18,4 +18,16 @@ def load_data():
             students = json.load(file)
     except:
         students = {}
+        #2. add_student
+        # GeeksforGeeks Tkinter tutorial
+        def add_student():
+            name = simpledialog.askstring("Input", "Enter student name:")
+            if not name:
+                return
+            if name in students:
+                messagebox.showerror("Error", "Student already exists")
+                return
+            students[name] = { "marks": [], "average": 0 }
+            update_table()
+            
 
