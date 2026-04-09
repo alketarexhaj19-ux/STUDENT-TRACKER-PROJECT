abbcd667f94fd14649dd72e2e90c1ce86fa04475
alketarexhaj19-ux <your.alketarexhaj19@gmail.com>
2026-04-09 19:38:01 +0100

    Added add_mark function with average calculation

--- student.py.py
+++ student.py.py
@@ -29,5 +29,22 @@ def load_data():
                 return
             students[name] = { "marks": [], "average": 0 }
             update_table()
+            #3. add_marks
+            #1) GeeksforGeeks Tkinter simpledialog tutorial
+            #2) Python statistics.mean() documentation
+            def add_marks():
+                name = simpledialog.askstring("Input", "Enter student name:")
+                if name not in students:
+                    messagebox.showerror("Error","Student not found")
+                    return
+                try:
+                    mark = float(simpledialog.askstring("Input", "Enter mark (0-100):"))
+                    if mark < 0 or mark >100:
+                        raise ValueError
+                    students[name]["marks"].append(mark)
+                    students[name]["average"] = round(statistics.mean(students[name]["marks"]), 2)
+                    update_table()
+                except:
+                    messagebox.showerror("Error", "Invalid input")
             
 
